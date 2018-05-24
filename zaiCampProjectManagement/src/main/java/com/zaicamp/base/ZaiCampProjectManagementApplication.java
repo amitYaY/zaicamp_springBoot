8116f34532c7554631c7a4f996b95379e6f9046b
Amit Upadhyay <aupadhyay@manh.com>
2018-05-25 00:56:36 +0530

    Removed unnecessary codes

--- zaiCampProjectManagement/src/main/java/com/zaicamp/base/ZaiCampProjectManagementApplication.java
+++ zaiCampProjectManagement/src/main/java/com/zaicamp/base/ZaiCampProjectManagementApplication.java
@@ -1,10 +1,16 @@
 package com.zaicamp.base;
 
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.zaicamp.base.project.domain.CoffeeMachine;
+import com.zaicamp.base.project.repos.CoffeeMachineRepository;
+import com.zaicamp.base.project.util.constants.MachineStatus;
 
 @SpringBootApplication
 @EnableBatchProcessing
@@ -14,5 +20,14 @@ public class ZaiCampProjectManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZaiCampProjectManagementApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner demo(CoffeeMachineRepository repository) {
+		return (args) -> {
+			CoffeeMachine machine = new CoffeeMachine();
+			machine.setStatus(MachineStatus.OFF.getStatus());
+			repository.save(machine);
+		};
 	}
 }
