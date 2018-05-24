99b15d499b76d0b3d564c5f9373d8c773d19d3cb
Amit Upadhyay <aupadhyay@manh.com>
2018-05-24 16:45:49 +0530

    Added First Entity

--- zaiCampProjectManagement/src/main/java/com/zaicamp/base/ZaiCampProjectManagementApplication.java
+++ zaiCampProjectManagement/src/main/java/com/zaicamp/base/ZaiCampProjectManagementApplication.java
@@ -3,9 +3,13 @@ package com.zaicamp.base;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableScheduling
+@EntityScan(basePackages= {"com.zaicamp.base.project.domain"})
 public class ZaiCampProjectManagementApplication {
 
 	public static void main(String[] args) {
